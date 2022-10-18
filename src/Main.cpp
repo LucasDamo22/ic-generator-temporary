9a44480a914b7ff50b7d1992b9c3313ffa8b89db
Lucas Damo <lucasmottadamo@gmail.com>
2022-10-18 07:06:12 -0300

    not-working

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,34 +5,47 @@
 #include <sstream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     unsigned seed = time(0);
     srand(seed);
+    int n_parcelas;
     int coef;
-    std::cout<<"digite um numero"<<std::endl;
-    std::cin>> coef;
-    int size = 0; 
-    size = rand()%coef;
-    std::cout<< size<< std::endl;
-    int vet[size];
-    
+    int sum = 0;
 
-    for(int i = 0;i<size;i++){
-        int aux=rand()%coef;
-        if(aux == 0){
-            std::cout <<"-------deuzero--------"<<std::endl;
-            aux=(1+rand()%coef);
+    coef = atoi(argv[1]);
+    n_parcelas = atoi(argv[2]);
+
+    std::vector<int> v1;
+
+    for (int i = 0; i < n_parcelas; i++)
+    {
+        int aux;
+
+        aux = rand() % (coef/2)+1;
+
+        if (sum < coef)
+        {
+            sum = sum + aux;
+            v1.push_back(aux);
+            
         }
-        vet[i]=aux;
-        coef--;
+    }
+    for (int i = 0; i < n_parcelas; i++)
+    {
+
+        std::cout << v1[i] << std::endl;
+    }
+    for (int i = 0; i < n_parcelas; i++)
+    {
+
+        double aux;
+        aux += v1[i];
+        std::cout << "\n\n\n\n"
+                  << aux / n_parcelas;
     }
 
-    for(int i = 0;i<size;i++){
-        std::cout<< vet[i]<<std::endl;
-    }
-   
     return 0;
 }
